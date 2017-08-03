7f191078031b9ec2db300741752258c8ab7d905f
Chao Li <chaoli@thinple.me>
2017-08-03 02:33:53 -0500

    Finish user sign up and log in

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts',
 ]
 
@@ -123,3 +124,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = [
     STATIC_DIR,
 ]
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
